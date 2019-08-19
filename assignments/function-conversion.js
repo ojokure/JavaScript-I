d37c8ca59aa38b24e54a9cde3145bc51f125b425
ojokure <ojokuredim@gmail.com>
2019-08-19 17:54:34 +0100

    converted ES5 syntax and convert to ES6 arrow Syntax

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,34 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+myFunction = () => console.log("Function was invoked!");
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+anotherFunction = (param) => console.log (param);
+anotherFunction("Example");
+
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+add = (param1, param2) => console.log (param1 + param2);
+add(1,2)
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+subtract = ( param1, param2) => console.log (param1 - param2);
+
+subtract(1,2);
 
 
 // Stretch
